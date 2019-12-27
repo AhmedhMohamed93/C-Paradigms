3c891ec8ac121f9c44758d0240d59c41be718749
AhmedhMohamed93 <54803368+AhmedhMohamed93@users.noreply.github.com>
2019-12-27 14:37:21 +0200

    Extensions and Lambdas expression

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,41 @@
+﻿using Extensions;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> cities = new[] { "Gent", "London", "Las Vegas", "Hyderabad" };
+            IEnumerable<string> query = cities.Filter((item) => item.StartsWith("L"));
+            foreach(var city in query)
+            {
+                Console.WriteLine(city);
+                Thread.Sleep(2000);
+            }
+        } 
+    }
+}
+
+namespace Extensions
+{
+    public static class FilterExtensions
+    {
+        public static IEnumerable<T> Filter<T> (this IEnumerable<T> input, FilterDelegate<T> predicate)
+        {
+            foreach(var item in input)
+            {
+                if(predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+            
+    }
+
+    public delegate bool FilterDelegate<T>(T item);
+}
