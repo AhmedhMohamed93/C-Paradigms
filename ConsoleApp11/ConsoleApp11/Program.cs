83852bf4b205ebee777d91e76e5cd20bafd8eddd
AhmedhMohamed93 <54803368+AhmedhMohamed93@users.noreply.github.com>
2019-12-27 15:46:36 +0200

    Func and Actions with lambdas expressions

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> Square = (x) => x * x;
+            Func<int, int, int> Add = (x, y) => (x + y);
+            Action<int> Write = x=> Console.WriteLine(x);
+            Write(Square(Add(5,2)));
+            
+            Thread.Sleep(1000);
+        }
+    }
+}
